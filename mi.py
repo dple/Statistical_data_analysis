5ec5ebfca8f129adfd6247b1c1feb5716e6a3644
Le Duc Phong <le.duc.phong@gmail.com>
2020-09-30 21:55:17 -0300

    Update mutual information

---
+++ mi.py
@@ -0,0 +1,35 @@
+"""
+Calculate mutual information between two variables
+
+Phong D. Le  -  le.duc.phong@gmail.com
+"""
+
+import numpy as np
+from entropy import single_entropy, joint_entropy
+from numpy.random import seed, randn, normal
+
+def mutual_information(X, Y, dist1=None, dist2 = None):
+    """
+    Calculate the mutual information between X and Y.
+    Using single and joint entropies
+
+    I(X, Y) = H(X) + H(Y) - H(X, Y)
+    """
+
+    HX = single_entropy(X, dist1)
+    HY = single_entropy(Y, dist2)
+    HXY = joint_entropy(X, Y, dist1, dist2)
+    mi = HX + HY - HXY
+
+    return mi
+    
+if __name__ == '__main__':
+
+    # seed random number generator
+    seed(1)
+    # prepare data
+    X = 20 * np.random.randn(1000) + 100
+    Y = X + (10 * np.random.randn(1000) + 50)
+    
+    mi = mutual_information(X, Y, 'unknown', 'normal')
+    print('Mutual Information between X and Y uing entropies, I(X, Y) = {}'.format(mi))
